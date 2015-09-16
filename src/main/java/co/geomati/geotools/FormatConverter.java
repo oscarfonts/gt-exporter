9a4eb0c6232d5e5543ecb6d7d2264dff5b57fa77
Oscar Fonts <oscar.fonts@gmail.com>
2015-09-16 16:16:01 +0200

    Code formatting

--- src/main/java/co/geomati/geotools/FormatConverter.java
+++ src/main/java/co/geomati/geotools/FormatConverter.java
@@ -17,36 +17,35 @@ import org.geotools.feature.SchemaException;
 import org.geotools.referencing.CRS;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.NoSuchAuthorityCodeException;
 
 public class FormatConverter {
 
 	public static void main(String[] args) {
 
-		Map<String,Object> srcParams = new HashMap<String,Object>();
+		Map<String, Object> srcParams = new HashMap<String, Object>();
 		String fileName = "/home/oscar/Projects/tmb/h2/docker/gs-h2/data_dir/TMB/AREA_TMB.shp";
-	    srcParams.put("url", DataUtilities.fileToURL(new File(fileName)));
+		srcParams.put("url", DataUtilities.fileToURL(new File(fileName)));
 
-		Map<String,Object> dstParams = new HashMap<String,Object>();
-	    dstParams.put("dbtype", "h2");
-	    dstParams.put("database", "H2_AREA_TMB");
-	    
-	    try {
+		Map<String, Object> dstParams = new HashMap<String, Object>();
+		dstParams.put("dbtype", "h2");
+		dstParams.put("database", "H2_AREA_TMB");
+
+		try {
 			DataStore src = DataStoreFinder.getDataStore(srcParams);
 			DataStore dst = DataStoreFinder.getDataStore(dstParams);
-			
+
 			copy(src, dst);
 
-	    } catch (IOException e) {
-			e.printStackTrace();
-		} finally {
 			System.out.println("Done!");
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	private static void copy(DataStore src, DataStore dst) throws IOException {
-	    Transaction t = new DefaultTransaction();
-	    try {
+		Transaction t = new DefaultTransaction();
+		try {
 			String typeName = src.getTypeNames()[0];
 			SimpleFeatureSource featureSource = src.getFeatureSource(typeName);
 
@@ -61,18 +60,17 @@ public class FormatConverter {
 			}
 			dst.createSchema(dstFt);
 			SimpleFeatureStore featureStore = (SimpleFeatureStore) dst.getFeatureSource(typeName);
-
-	    	SimpleFeatureCollection collection = featureSource.getFeatures();
-	    	featureStore.setTransaction(t);
-	    	featureStore.addFeatures(collection);
-	        t.commit();
-	    } catch (IOException e) {
-            t.rollback();
-	    } finally {
-	        t.close();
-	        dst.dispose();
-	        src.dispose();
-	    }
+			SimpleFeatureCollection collection = featureSource.getFeatures();
+			featureStore.setTransaction(t);
+			featureStore.addFeatures(collection);
+			t.commit();
+		} catch (IOException e) {
+			t.rollback();
+		} finally {
+			t.close();
+			dst.dispose();
+			src.dispose();
+		}
 	}
 
 }
